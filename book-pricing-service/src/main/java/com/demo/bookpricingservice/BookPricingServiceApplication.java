9120cd8b42d6cfcf2d954f82d329f3efa3c5aee7
Georges Mbella-Essomba <fred_leschener@hotmail.com>
2020-01-14 06:32:20 +0000

    microservices application under the hood

---
+++ book-pricing-service/src/main/java/com/demo/bookpricingservice/BookPricingServiceApplication.java
@@ -0,0 +1,14 @@
+package com.demo.bookpricingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPricingServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BookPricingServiceApplication.class, args);
+	}
+
+}
---
+++ book-pricing-service/src/main/java/com/demo/bookpricingservice/models/Pricing.java
@@ -0,0 +1,28 @@
+package com.demo.bookpricingservice.models;
+
+public class Pricing {
+    private String isbn;
+    private int price;
+
+
+    public Pricing(String isbn, int price) {
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
